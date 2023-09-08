ec9f4fbc6129f9f4ed6896bf473318ca960a52d0
percy1997 <percy_01_1997@outlook.com>
2023-09-07 22:15:58 -0500

    Crear proyecto SistemaCreditoYape

---
+++ src/main/java/com/credito/SistemaCreditoYapeApplication.java
@@ -0,0 +1,13 @@
+package com.credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCreditoYapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCreditoYapeApplication.class, args);
+	}
+
+}
