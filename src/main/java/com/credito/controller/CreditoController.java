f0f92a276a9bfa2d98edc1e0446f3da0fc1175c2
percy1997 <percy_01_1997@outlook.com>
2023-09-09 16:27:52 -0500

    cambios menores

--- src/main/java/com/credito/controller/CreditoController.java
+++ src/main/java/com/credito/controller/CreditoController.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.credito.entity.Credito;
 import com.credito.entity.CreditoUsuario;
 import com.credito.entity.Interes;
 import com.credito.entity.Usuario;
